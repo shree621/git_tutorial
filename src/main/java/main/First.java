902477f1a968fa457ff4e2ecf1b1449d08d129d6
shree621 <ssshree820@gmail.com>
2026-07-12 21:53:17 +0530

    main added

--- src/main/java/main/First.java
+++ src/main/java/main/First.java
@@ -4,7 +4,9 @@ public class First {
 	
 	public static void main(String[] args) {
 		
-		
+		int a=10,b=20,add;
+		add=a+b;
+		System.out.println("addition="+add);
 		System.out.println("hello world");
 	}
 
