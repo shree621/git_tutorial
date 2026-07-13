c274d2d023cc7e8853c02870b6ad984ea4d697b1
shree621 <ssshree820@gmail.com>
2026-07-13 18:46:18 +0530

    mul commit

--- src/main/java/main/First.java
+++ src/main/java/main/First.java
@@ -2,12 +2,22 @@ package main;
 
 public class First {
 	
+	   void mul()
+	   {
+		   int x=58;
+		  int  y=20;
+		   System.out.println("X="+x+" "+"Y="+y);
+	   }
+	
 	public static void main(String[] args) {
 		
 		int a=10,b=20,add;
 		add=a+b;
 		System.out.println("addition="+add);
 		System.out.println("hello world");
+		
+		First f=new First();
+		f.mul();
 	}
 
 }
